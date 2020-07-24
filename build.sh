2646233230985b2da0881aa5bd4a99a778f4ba3a
Alexandr Savca <alexandr.savca89@gmail.com>
2020-07-24 15:22:16 +0300

    clean build.sh script

--- build.sh
+++ build.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
 CCACHE=$(command -v ccache)
-
-TOOLCHAIN=/home/adesikha15/aarch64-linux-android/bin/aarch64-cortex_a53-linux-android-
+TOOLCHAIN=$PWD/../toolchain64/bin/aarch64-linux-android-
 
 export CROSS_COMPILE="${CCACHE} ${TOOLCHAIN}"
-
 export ARCH=arm64
-
-export KBUILD_BUILD_USER=adesh15
-export KBUILD_BUILD_HOST=reactor
 
 make clean O=out/
 make mrproper O=out/
-
 make mido_defconfig O=out/
-
-make -j8 O=out/
+make -j$(nproc) O=out/
